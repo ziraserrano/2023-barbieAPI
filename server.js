bd20fd415b8fd2c89821d43e47f627c1a65975e3
zira serrano <ziraserrano.dev@gmail.com>
2023-07-18 12:46:40 -0400

    Add get req & start barbies API

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const barbies = {
+    'Barbie':{
+        'barbieName': 'Barbie',
+        'actorName': 'Margot Robbie',
+        'barbieQuote': 'testing 123',
+        'barbieImg': 'x'
+    },
+    'Ken':{
+        'barbieName': 'Ken',
+        'actorName': 'Ryan Gosling',
+        'barbieQuote': 'testing 123',
+        'barbieImg': 'x'
+    }
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:barbies', (req, res) => {
+    const barbiesNames = req.params.barbies  //set variable to barbiesNames that requests the entire barbies object from the url
+    if(barbies[barbiesNames]){  //barbies is the object + barbiesNames is the variable
+        res.json(barbies[barbiesNames]) //response with whatever the specific barbie's info is
+    }else{
+        console.log('Not Found')
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on PORT ${PORT}`)
+})
