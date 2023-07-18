c1e8c08c9ba6363179dab9c8dedf5f2a29d05c00
zira serrano <ziraserrano.dev@gmail.com>
2023-07-18 12:46:52 -0400

    Move js out of public folder

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// document.querySelector('button').addEventListener('click', makeReq)
+
+// async function makeReq(){
+//     const rapper = document.querySelector('input').value
+//     try{
+//         const response = await fetch('') // have to connect to cyclic first and then add 
+//         const data = await response.json()
+
+//         console.log(data)
+//         document.querySelector('h2').innerText = data.name
+//     }
+//     catch(error => console.error(error))
+// }
