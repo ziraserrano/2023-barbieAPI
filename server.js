9b00167b6b17a3f5f04965f2eced9367ae4fb3e9
zira serrano <ziraserrano.dev@gmail.com>
2023-08-02 15:03:24 -0400

    Finish adding to the API

--- server.js
+++ server.js
@@ -11,12 +11,92 @@ const barbies = {
     'barbie':{
         'barbieName': 'Barbie',
         'actorName': 'Margot Robbie',
-        'barbieQuote': 'testing 123'
+        'barbieQuote': 'no quote found'
     },
     'ken':{
-        'barbieName': 'Ken',
-        'actorName': 'Ryan Gosling',
-        'barbieQuote': 'testing 123'
+        'barbieName': 'Ken, Ken, Ken',
+        'actorName': 'Ryan Gosling, Scott Evans, Kingsley Ben-Adir, Simu Liu, Ncuti Gatwa',
+        'barbieQuote': 'no quote found'
+    },
+    'allan':{
+        'barbieName': 'Allan',
+        'actorName': 'Michael Cera',
+        'barbieQuote': 'no quote found'
+    },
+    'author':{
+        'barbieName': 'Celebrated Author',
+        'actorName': 'Alexandra Shipp',
+        'barbieQuote': 'no quote found'
+    },
+    'diplomat':{
+        'barbieName': 'Diplomat',
+        'actorName': 'Nicola Coughlan',
+        'barbieQuote': 'no quote found'
+    },
+    'doctor':{
+        'barbieName': 'Doctor',
+        'actorName': 'Hari Nef',
+        'barbieQuote': 'no quote found'
+    },
+    'humans':{
+        'barbieName': 'human',
+        'actorName': 'Ariana Greenblatt, America Ferrera, Will Ferrell',
+        'barbieQuote': 'no quote found'
+    },
+    'intern':{
+        'barbieName': 'intern',
+        'actorName': 'Connor Swindells',
+        'barbieQuote': 'no quote found'
+    },
+    'lawyer':{
+        'barbieName': 'lawyer',
+        'actorName': 'Sharon Rooney',
+        'barbieQuote': 'no quote found'
+    },
+    'mermaid':{
+        'barbieName': 'mermaid',
+        'actorName': 'Dua Lipa',
+        'barbieQuote': 'no quote found'
+    },
+    'midge':{
+        'barbieName': 'midge',
+        'actorName': 'Emerald Fennell',
+        'barbieQuote': 'no quote found'
+    },
+    'narrator':{
+        'barbieName': 'narrator',
+        'actorName': 'Helen Mirren',
+        'barbieQuote': 'no quote found'
+    },
+    'noble prize':{
+        'barbieName': 'nobel prize',
+        'actorName': 'Emma Mackey',
+        'barbieQuote': 'no quote found'
+    },
+    'president':{
+        'barbieName': 'president',
+        'actorName': 'Issa Rae',
+        'barbieQuote': 'no quote found'
+    },
+    'pulitzer':{
+        'barbieName': 'pulitzer',
+        'actorName': 'Ritu Arya',
+        'barbieQuote': 'no quote found'
+    },
+    'supreme court justice':{
+        'barbieName': 'supreme court justice',
+        'actorName': 'Ana Cruz Kayne',
+        'barbieQuote': 'no quote found'
+    },
+    'split':{
+        'barbieName': 'split',
+        'actorName': 'Kate McKinnon',
+        'barbieQuote': 'no quote found'
+    },
+    'suit':{
+        'barbieName': 'suit',
+        'actorName': 'Jamie Demetriou',
+        'barbieQuote': 'no quote found'
     }
 }
 
